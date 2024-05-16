d14e145d4f023ecd3edfe6945749a172140d40dc
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-16 18:29:48 +0530

    5

---
+++ project1/src/test/java/pro1test/Kingtest.java
@@ -0,0 +1,29 @@
+package pro1test;
+
+import org.testng.annotations.Test;
+
+public class Kingtest {
+	@Test
+	public void test25test()
+	{
+	/**	String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PAssword = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PAssword);**/
+		
+		
+		System.out.println("1111111");
+		System.out.println("print one c");
+	}
+
+	@Test
+	public void test26test()
+	{
+		System.out.println("print one c");
+	}
+
+}
---
+++ project1/src/test/java/protest/King2test.java
@@ -0,0 +1,24 @@
+package protest;
+
+import org.testng.annotations.Test;
+
+public class King2test {
+	public class Test23 {
+		@Test
+		public void test25test()
+		{
+			System.out.println("print one d");
+		}
+
+		@Test
+		public void test26test()
+		{
+			System.out.println("print one h");
+		}
+
+		
+		
+
+}
+
+}
