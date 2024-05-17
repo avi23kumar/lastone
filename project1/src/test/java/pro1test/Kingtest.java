ac34e62e9c8bb71827f9cac54c26a29ec261d93c
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-17 09:33:31 +0530

    5

--- project1/src/test/java/pro1test/Kingtest.java
+++ project1/src/test/java/pro1test/Kingtest.java
@@ -6,16 +6,14 @@ public class Kingtest {
 	@Test
 	public void test25test()
 	{
-	/**	String URL = System.getProperty("url");
+		String URL = System.getProperty("url");
 		String BROWSER = System.getProperty("browser");
 		String USERNAME = System.getProperty("username");
 		String PAssword = System.getProperty("password");
 		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(USERNAME);
-		System.out.println(PAssword);**/
-		
-		
+		System.out.println(PAssword);
 		System.out.println("1111111");
 		System.out.println("print one c");
 	}
